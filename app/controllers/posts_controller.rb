ec5b38b8d1adfba747b63415efa153e0f56ca93f
leehanhom <leehanhom@hotmail.com>
2020-12-03 00:53:12 +0800

    1203 dcard practice

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
+  before_action :session_requires, only: [:create]
+  before_action :set_board, only: [:new, :create]
   def new
+    @post = Post.new
     
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.board = @board
+    @post.user = current_user
+
+    if @post.save
+      redirect_to root_path, notice: '新增文章成功'
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_board
+    @board = Board.find(params[:board_id])
+  end
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
